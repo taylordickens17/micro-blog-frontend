8917462a40f878489f25e77c2196cce25d746769
Taylor Dickens <59926636+taylordickens17@users.noreply.github.com>
2022-08-31 20:31:55 -0400

    Added Individual Post Routing

---
+++ src/post.config.js
@@ -0,0 +1,11 @@
+export const posts = [
+  { id: 1, title: "Test Post", location: "/posts/test.md" },
+  { id: 2, title: "Second Test", location: "/posts/test2.md" },
+  { id: 3, title: "THIRD MUTHAFUCKIN TEST", location: "/posts/test3.md" },
+];
+
+{
+  /* {posts.map((post) => {
+        return <Post key={post.id} postUrl={post.location} />;
+      })} */
+}
